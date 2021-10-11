85f7e634236b86057165c70af3c07b2c2e8d348c
12tall <fb.ouyang@outlook.com>
2021-10-11 21:35:48 +0800

    重构代码

--- README.md
+++ README.md
@@ -1,12 +1,61 @@
-一次基于`wry`的本地可执行程序模板，以`vue3`作为前端、`rust`作为后端的尝试。至于为什么不用`Electron`或者`tauri`：
-  - `Electron`打包后的文件体积比较大，且资源文件不能集成到可执行程序；
-  - `tauri`虽然功能丰富，但是学习起来不如更底层的实现容易。
+# 燕/yan  
 
-项目名称源自唐代诗人刘禹锡的《乌衣巷》：`旧时王谢堂前燕 飞入寻常百姓家`。
+`旧时王谢堂前燕 飞入寻常百姓家`——《乌衣巷》唐·刘禹锡。  
+
+此项目是一个基于`rust`和`webview`的模板。后端采用`wry`，前端采用`vue3`作为骨架，前后端通过IPC 进程通信。至于为什么不直接用`Electron`或者`tauri`？首先`Electron`打包后的文件体积比较大，但是打包后的结果文件不够简洁；而`tauri`由于封装程度比较高，并不适合拿来学习理解整个项目的实现原理。
+
+当然，通过`webview(2)`也有一些瑕疵，即无法直接调用平台的某些api、需要自己设置`user_data_directory`的位置。这些都是值得的，因为这些问题即使这次不遇到，以后也可能会遇到。就像《天月神话》里面说的，根本就没有银弹。
+
+-----
+
+## 说明  
+由于没有嵌入`Chromium`，编译后的结果文件会比较小；但是这也导致要求系统必须已经预装了`Webview`运行时。当然也可以通过`set_env`来指定固定版本的`webview(2)`运行时，但是这样的编译结果就违背了简洁的初衷。另外，某些跨平台的代码，需要添加宏命令来实现条件编译。
+
+### Rust 后端
+1. `Object`对象可以通过`get`方法访问键值；
+2. 修改图标：需要引用`ico`库，详细代码参见[wry/examples/icon.rs](https://github.com/tauri-apps/wry/blob/6ffd1d7194bda9ca1434fa2ca0d0bd0c8237f01f/examples/icon.rs)，需要注意的是源代码没有包含在`wry`库中，且有些类型已重新命名。
+3. 条件编译
+  - `#[cfg(debug_assertions)]`：判断程序是否处于debug 状态，可用于任何语句块之前
+  - `#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]`，添加在`main.rs`最前面
+
+
+### Vue前端
+1. `vscode`的插件不能正确识别`web_srv`子项目，所以最好能在新的IDE 窗口修改前端部分；
+2. 前端事件的监听器可以是异步函数；
+
+
+## 依赖的库  
+不必过于担心引入过多的库而导致程序运行效率低下，因为：
+  1. 事情总要做，不是自己做就是别人做；
+  2. 编译器会帮我们优化；
+  3. 开发效率也很重要，并且引入三方库让代码更整洁。
+
+### rust 库  
+
+序号|名称|说明  
+:---:|:---|:---  
+01|Geal/nom|二进制文件解析  
+02|WLBF/single-instance|跨平台应用单例模式  
+03|BurntSushi/byteorder|Read trait 扩展，可用于二进制文件解析，比nom 更基础  
+04|tauri-app/wry|基于tao，支持webview2，生成桌面应用  
+05|Michael-F-Bryan/include_dir|将文件夹以二进制的形式打包进exe
+06|PolyMeilex/rfd|跨平台的对话框库  
+07|hyperium/mime|MIME 文件类型支持  
+08|retep998/winapi-rs|win32api 的封装
+09|microsoft/windows-rs|微软官方库，但有些东西不全  
+10|mdsteele/rust-ico|ico 图标文件支持
+
+### js 库  
+
+序号|名称|说明  
+:---:|:---|:---  
+01|vue3|前端框架
+02|jiggzson/nerdamer|数学符号计算库
+
 
 ## 调试与打包  
-
 ### 调试  
+调试模式下，需要启动一个vue 的web 服务，可以实现前端代码修改的实时同步。
 ```bash
 # 1. 启动vue（需要新开一个控制台）
 cd web_src
@@ -17,7 +66,8 @@ yarn dev  # 此进程会阻塞
 cargo run
 ```
 
-### 打包  
+### 发布  
+发布模式下，需要先编译前端vue 代码，然后再编译rust 代码。
 ```bash
 # 1. 打包web 页面
 cd web_src  
@@ -28,47 +78,3 @@ yarn build
 cd ..  # 切换回根目录
 cargo build --release
 ```
-
-## Vue前端
-1. 创建`vue3`项目：`yarn create vite web_src --template vue`；  
-2. 在`vite.config.js`中修改输出目录；  
-```js
-// vite.config.js
-export default defineConfig({
-  // ... ,
-  build: {
-    outDir: '../web',  // 打包结果输出目录
-  },
-  server: {
-    port: 8341,        // 指定调试服务的的各种信息
-  }
-})
-```
-3. 项目有一些缺点，当然也有一个优点：由于是调用系统的`webview`功能，打包后的可执行文件体积非常小！
-    - `vscode`的插件不能正确识别`web_srv`子项目，所以最好能在新的IDE 窗口修改前端部分。
-    - 有些跨平台的代码需要手动添加宏操作，例如图标等资源文件打包、窗口管理等
-4. 前端事件的监听器可以是异步函数
-
-## Rust后端
-1. 以`wry`作为后端：`wry = "0.12.2"`；  
-2. 将`web`作为二进制文件包含至`exe`：`include_dir = "0.6.2"`；
-3. `webview`不能直接在前端获取本地文件路径，所以需要`rfd`来从后端选择并处理文件；
-4. 根据文件后缀名获取`MIME`类型：`mime = "0.3.16"`；  
-5. PE 文件在`windows.rs`中有定义，基本上所有的类型在这个`binding`中都能找到；
-6. 通过`rfd`调用系统的对话框，但是需要根据平台去设置初始路径、或者干脆不设置；
-7. `Object`对象可以通过`get`方法访问键值；
-8. 修改图标：需要引用`ico`库，详细代码参见[wry/examples/icon.rs](https://github.com/tauri-apps/wry/blob/6ffd1d7194bda9ca1434fa2ca0d0bd0c8237f01f/examples/icon.rs)，需要注意的是源代码没有包含在`wry`库中，且有些类型已重新命名。
-9. 引入`byteorder = "1.4.3"`以二进制的形式读取文件，并可以区分大小端字节序，记得要先将`File`封装为`BufReader`
-
-
-### 条件编译  
-
-1. 调试状态
-    - `#[cfg(debug_assertions)]`：判断程序是否处于debug 状态  
-    - `#[cfg(not(debug_assertions))]`：可以用于任何语句（块）前面  
-2. 取消Windows 下的命令行窗口：
-    - 在`main.rs`最上方添加：`#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]`
-
-
-### 有用的库
-1. [二进制文件解析](https://github.com/Geal/nom)
--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,28 @@
-// use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
-// use std::{
-//     fs::File,
-//     io::{self, BufReader, Read, Seek, SeekFrom},
-//     mem
-// };
-// use std::env;
-// // representation 描述、表示
-// // packaged 表示紧密排列
-// #[repr(C, packed)]
-// #[derive(Debug)]
-// struct DosHeader {
-//     magic: [u8; 2],
-// }
-// fn main() {
-//     env::set_var(
-//         "WEBVIEW2_BROWSER_EXECUTABLE_FOLDER",
-//         "./",
-//     );
-//     let mut pe = File::open("yan.exe").unwrap();
-//     let mut dh = unsafe { mem::zeroed::<DosHeader>() };
-//     let mz = pe.read_exact(& mut dh.magic);
-//     // let mz=  std::ascii::escape_default(mz);
-
-//     println!("{:X?}", dh.magic);
-// }
-
+// release 版，关闭控制台窗口
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{env, io::Cursor, path::PathBuf};
+mod utils;
+use std::io::Result;
 
-use ico::*;
 use rfd::FileDialog;
+use utils::{check_process_mutex, read_icon};
 use wry::{
-    self,
     application::{
         event::{Event, StartCause, WindowEvent},
-        event_loop::{ControlFlow, EventLoop},
-        window::{Icon, Window, WindowBuilder},
+        event_loop::ControlFlow,
+        window::Window,
     },
-    http::Response,
-    webview::{RpcRequest, RpcResponse, WebContext, WebViewBuilder},
+    webview::{RpcRequest, RpcResponse},
     Value,
 };
-fn main() {
-    // 使用指定版本的Webview2
-    // 前提是要用expand 指令解压cab 文件
-    // cab 文件需要在https://developer.microsoft.com/en-us/microsoft-edge/webview2/#download-section 下载
-    // env::set_var(
-    //     "WEBVIEW2_BROWSER_EXECUTABLE_FOLDER",
-    //     "D:\\code\\yan\\wv2",
-    // );
 
-    // 1. 创建窗口事件循环对象
-    let evl = EventLoop::new();
+use crate::utils::{create_app, create_webview};
 
-    // use a window icon
-    let by = include_bytes!("../logo128.ico");
-    let icon_dir = IconDir::read(Cursor::new(by)).unwrap();
-    let image = icon_dir.entries()[0].decode().unwrap();
-    let rgba = image.rgba_data();
-    let icon = Icon::from_rgba(rgba.to_vec(), image.height(), image.width()).unwrap();
-
-    // 2. 创建窗口并绑定事件循环
-    let win = WindowBuilder::new()
-        .with_title("My App")
-        .with_window_icon(Some(icon))
-        .build(&evl)
-        .unwrap();
-    // 3. 在本地窗口种创建webview，并加载
-    let web = WebViewBuilder::new(win).unwrap();
-
-    // 调试模式下加载url
-    #[cfg(debug_assertions)]
-    let web = web.with_url("http://localhost:8341/").unwrap();
-
-    // release 模式下加载html 文件
-    // 需要自定义协议的支持
-    #[cfg(not(debug_assertions))]
-    let web = {
-        use include_dir::{include_dir, Dir};
-        use wry::http::{status::StatusCode, ResponseBuilder};
-        const WEB_DIR: Dir = include_dir!("./web");
-        web.with_custom_protocol("wry".into(), move |req| {
-            // 1. 获取请求的url，
-            // 如果协议格式为wry://index.html，程序会把index.html 做路径处理
-            let path = req.uri().replace("wry://web/", "");
-
-            // 2. 获取文件内容并设置响应类型
-            let (code, data, meta) = match WEB_DIR.get_file(&path) {
-                Some(file) => match path.split(".").last().unwrap() {
-                    "html" => (200, file.contents(), mime::TEXT_HTML),
-                    "js" => (200, file.contents(), mime::TEXT_JAVASCRIPT),
-                    "css" => (200, file.contents(), mime::TEXT_CSS),
-                    "ico" | "png" => (200, file.contents(), mime::IMAGE_PNG),
-                    "jpg" => (200, file.contents(), mime::IMAGE_JPEG),
-                    "bmp" => (200, file.contents(), mime::IMAGE_BMP),
-                    _ => (404, "not found".as_bytes(), mime::TEXT_PLAIN),
-                },
-                None => (404, "not found".as_bytes(), mime::TEXT_PLAIN),
-            };
-
-            // 3. 构建返回体返回
-            ResponseBuilder::new()
-                .status(StatusCode::from_u16(code).unwrap())
-                .mimetype(meta.essence_str())
-                .body(Vec::from(data))
-        })
-        // 见步骤1.
-        .with_url("wry://web/index.html")
-        .unwrap()
-    };
-
-    let handler = move |window: &Window, mut req: RpcRequest| {
+fn main() -> Result<()> {
+    let _inst = check_process_mutex()?;
+    let icon = read_icon(include_bytes!("../res/logo128.ico"));
+    let (win, evl) = create_app(icon);
+    let webview = create_webview(win, move |_window: &Window, mut req: RpcRequest| {
         let mut response = None;
         // 解析rpc 的参数
         println!("{:?}", req.params?.get(0).unwrap().get("message"));
@@ -129,21 +40,8 @@ fn main() {
             ));
         }
         response
-    };
+    });
 
-    // 自定义user_data_directory 路径
-    // 否则会在exe 下面出现一个文件夹，比较烦人
-    // 还需要添加路径是否存在的判定
-    let mut user_data_dir = PathBuf::from(env::var("appdata").unwrap());
-    user_data_dir.push("yan");
-    let mut webctx = WebContext::new(Some(user_data_dir));
-    let mut web = web
-        .with_rpc_handler(handler)
-        .with_web_context(&mut webctx)
-        .build()
-        .unwrap();
-
-    // 4. 设置事件处理
     evl.run(move |evt, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
@@ -156,11 +54,14 @@ fn main() {
             } => match event {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                 WindowEvent::Resized(_) => {
-                    let _ = web.resize();
+                    let _ = webview.resize();
                 }
                 _ => (),
             },
             _ => (),
         }
     });
+
+    // Ok(())
 }
+
